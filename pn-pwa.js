836dc169f899b0f3ac08fca6cfd16ba7fe9c4613
zhami15 <94533825+zhami15@users.noreply.github.com>
2026-09-05 21:46:04 +0600

    Delete pn-pwa.js

--- pn-pwa.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  'use strict';
-  if(!('serviceWorker' in navigator))return;
-  window.addEventListener('load',()=>{
-    navigator.serviceWorker.register('./sw.js',{scope:'./'}).catch(err=>console.warn('ProfiNavi service worker:',err));
-  });
-})();
